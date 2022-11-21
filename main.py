4cff02984cfa2c72f6c728428d30bc5ed638fef8
Eric Kopen <erictkopen@gmail.com>
2022-11-21 14:13:02 -0600

    pf tracker!

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-def stock_data(stocks, start_date, end_date, investment):
+def stock_data(stocktickers, stock_names, start_date, end_date, investment):
     stock_dict = {}
-    for stock in stocks:
+    for (stock, name) in zip(stocktickers, stock_names):
 
         stock_df = pd.DataFrame(yf.download(stock, start_date, end_date))
         stock_df['daily_return'] = np.log(stock_df['Adj Close'] / stock_df['Adj Close'].shift(1))
@@ -17,16 +17,17 @@ def stock_data(stocks, start_date, end_date, investment):
             running_investment = running_investment * (1+stock_df['daily_return'][days])
             investment_series.append(running_investment)
         stock_df['portfolio value'] = investment_series
-        stock_dict[stock] = stock_df['portfolio value']
+        stock_dict[name] = stock_df['portfolio value']
 
     return pd.DataFrame(stock_dict)
 
-healthcare_stocks = ['UNH', 'CI', 'ELV', 'CNC', 'HUM']
+healthcare_stock_tickers = ['UNH', 'CI', 'ELV', 'CNC', 'HUM']
+healthcare_stock_names = ["United Health", "Cigna", "Elevance", "Centene", "Humana"]
 start = '2010-01-01'
 end = '2019-12-31'
 starting_amount = 1000
 
-portfolio = stock_data(healthcare_stocks, start, end, starting_amount)
+portfolio = stock_data(healthcare_stock_tickers, healthcare_stock_names, start, end, starting_amount)
 print(portfolio)
 portfolio.plot()
 plt.show()
