6f6a5aa3e5c0e36a5fab2f376939a6e155101d11
Eric Kopen <erictkopen@gmail.com>
2022-11-23 00:46:23 -0600

    stock trading simulator!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+# create a data frame with daily returns (unadjusted) by stock ticker
 def daily_returns(stocktickers, start_date, end_date):
 
     stock_dict = {}
@@ -14,8 +15,8 @@ def daily_returns(stocktickers, start_date, end_date):
 
     return pd.DataFrame(stock_dict)
 
-def long_strat(stock_df, stocktickers, investment):
-#can probably get rid of the need to reimport tickers
+# run the daily returns data frame through different simple investment strategies
+def calc_portfolio_perfomance(stock_df, stocktickers, investment, investment_strat):
 
     stock_dict = {}
     for ticker in stocktickers:
@@ -24,76 +25,57 @@ def long_strat(stock_df, stocktickers, investment):
         running_investment = investment
 
         for days in range(1,len(stock_df)):
-            running_investment = running_investment * (1+stock_df[ticker][days])
-            investment_series.append(running_investment)
+            if investment_strat == "long_strat":
+                # buy and hold
+                running_investment = running_investment * (1+stock_df[ticker][days])
+                investment_series.append(running_investment)
+            if investment_strat == "positive_trend_strat":
+                # if stock goes down the previous day, take out investment. If not, reinvest.
+                if stock_df[ticker][(days - 1)] < 0:
+                    running_investment = running_investment
+                else:
+                    running_investment = running_investment * (1 + stock_df[ticker][days])
+                investment_series.append(running_investment)
+            if investment_strat == "anti_trend_strat":
+                # if stock goes up the previous day, take out investment. If not, reinvest.
+                if stock_df[ticker][(days - 1)] > 0:
+                    running_investment = running_investment
+                else:
+                    running_investment = running_investment * (1 + stock_df[ticker][days])
+                investment_series.append(running_investment)
         stock_df['portfolio value'] = investment_series
         stock_dict[ticker] = stock_df['portfolio value']
 
     return pd.DataFrame(stock_dict)
 
-def positive_trend_strat(stock_df, stocktickers, investment):
-
-    stock_dict = {}
-    for ticker in stocktickers:
-
-        investment_series = [investment]
-        running_investment = investment
-
-        for days in range(1, len(stock_df)):
-            # #if stock goes down the previous day, take out investment. If not, reinvest.
-            if stock_df[ticker][(days-1)] < 0:
-                running_investment = running_investment
-            else:
-                running_investment = running_investment * (1 + stock_df[ticker][days])
-            investment_series.append(running_investment)
-        stock_df['portfolio value'] = investment_series
-        stock_dict[ticker] = stock_df['portfolio value']
-
-    return pd.DataFrame(stock_dict)
-
-def anti_trend_strat(stock_df, stocktickers, investment):
-
-    stock_dict = {}
-    for ticker in stocktickers:
-
-        investment_series = [investment]
-        running_investment = investment
-
-        for days in range(1, len(stock_df)):
-            #if stock goes up the previous day, take out investment. If not, reinvest.
-            if stock_df[ticker][(days-1)] > 0:
-                running_investment = running_investment
-            else:
-                running_investment = running_investment * (1 + stock_df[ticker][days])
-            investment_series.append(running_investment)
-        stock_df['portfolio value'] = investment_series
-        stock_dict[ticker] = stock_df['portfolio value']
-
-    return pd.DataFrame(stock_dict)
-
-
-
-
+# initialize the portfolio
 portfolio_stock_tickers = ['JNJ', 'LLY', 'PFE', 'ABBV', 'MRK']
-# healthcare_stock_names = ["United Health", "Cigna", "Elevance", "Centene", "Humana"]
-start = '2015-01-01'
+start = '2005-01-01'
 end = '2019-12-31'
+# maybe clarify this is per stock, create ways to weigh it
 starting_amount = 1000
 
-
+# get the returns data frame and serialize it before passing through future functions
 portfolio_returns = daily_returns(portfolio_stock_tickers, start, end)
 portfolio_returns.to_pickle("./daily_returns.pkl")
 print(portfolio_returns)
 
-long_portfolio = long_strat(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers, starting_amount).sum(axis=1)
-positive_trend_portfolio = positive_trend_strat(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers, starting_amount).sum(axis=1)
-anti_trend_portfolio = anti_trend_strat(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers, starting_amount).sum(axis=1)
+# create data frames for the different strategies and return a sum column
+# this could be looped for better efficiency
+long_portfolio = calc_portfolio_perfomance(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers,
+                                                 starting_amount, "long_strat").sum(axis=1)
+positive_trend_portfolio = calc_portfolio_perfomance(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers,
+                                                starting_amount, "positive_trend_strat").sum(axis=1)
+anti_trend_portfolio = calc_portfolio_perfomance(pd.read_pickle("./daily_returns.pkl"), portfolio_stock_tickers,
+                                                    starting_amount, "anti_trend_strat").sum(axis=1)
 
+# combine into a single data frame and plot it
 portfolios = [long_portfolio, positive_trend_portfolio, anti_trend_portfolio]
 portfolio_combos = pd.concat(portfolios, keys=["Long Only", "Trend Follow", "Trend Reverse"], axis = 1, join="inner")
 print(portfolio_combos)
-
 portfolio_combos.plot()
 plt.show()
+
+# work on further interpreting this part
 
 
