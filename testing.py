5ca37f97b7dab42abdece171d1839ef93f1de284
Eric Kopen <erictkopen@gmail.com>
2022-11-27 19:03:10 -0600

    more logical dataframe steps

--- testing.py
+++ testing.py
@@ -1,25 +1,33 @@
-"""just a file to test random shtuff
-    """
-
 import numpy as np
 import pandas as pd
 from numpy import mean
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-def stock_data(stockticker, start_date, end_date):
+stockticker = 'WMT'
+historic_start = '2000-01-01'
+start = '2015-01-02'
+end = '2019-12-31'
+
+def stock_historical_data(stockticker, start_date, end_date):
+    #initialize the stock data from Yahoo Finance
 
     stock_dict = {}
     stock_df = pd.DataFrame(yf.download(stockticker, start_date, end_date))
     stock_df['daily_return'] = np.log(stock_df['Adj Close'] / stock_df['Adj Close'].shift(1))
-    stock_dict[stockticker] = stock_df[['Open', 'Adj Close', 'daily_return']].iloc[252:]
-    return (stock_dict)
+    return stock_df.reset_index()
 
-print
+def stock_data_in_range(stock_df, start):
+    #these locs are unneeded. not sure why.
+    indexnum = stock_df.loc[stock_df['Date'] == start].index.values
+    return stock_df.loc[int(indexnum):]
 
-ticker = 'IBM'
-start = '2009-01-01'
-end = '2019-12-31'
-investment = 1000
+#create rolling returns columns
 
-print(stock_data(ticker, start,end)['IBM']['daily_return'])
+
+hist_data = stock_historical_data('SPY', historic_start, end)
+# this loc is unneeded. not sure why.
+print(hist_data.loc[hist_data['Date'] == '2015-01-02'])
+
+stock_data = stock_data_in_range(hist_data, start)
+print(stock_data)
